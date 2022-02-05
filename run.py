32babf960d60033fa09dfa337882c932729d6fde
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-02-05 22:08:38 +0000

    add random impoort to be used for the game against the computer

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# imports
+import random 
 field = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 xoro = ["X", "O"]
 winner = None
 play_game = True
+
 
 
 # game name
