bb96444486968b1f27a75c71bf4c34fe733e7737
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-02-05 20:36:42 +0000

    add game field function and the chart to the readme file

--- run.py
+++ run.py
@@ -1,10 +1,24 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-field = ["-", "-", "-",
-        "-", "-", "-",
-        "-", "-", "-"]
-
+field = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 player = "X"
 winner = None
 play_game = True
+
+
+
+
+# game field
+def print_field(field):
+    print(" Game Field" + " "*9 + "Reference field")
+    print(" " + field[1] + " | " + field[2] + " | " + field[3] + "  " +
+          " "*10 + " " + "1" + " | " + "2" + " | " + "3" + "  ")
+    print("---|---|---" + " "*11 + "---|---|---")
+    print(" " + field[4] + " | " + field[5] + " | " + field[6] + "  " +
+          " "*10 + " " + "4" + " | " + "5" + " | " + "6" + "  ")
+    print("---|---|---" + " "*11 + "---|---|---")
+    print(" " + field[7] + " | " + field[8] + " | " + field[9] + "  " +
+          " "*10 + " " + "7" + " | " + "8" + " | " + "9" + "  ")
+    print("\n")
+print_field(field)
